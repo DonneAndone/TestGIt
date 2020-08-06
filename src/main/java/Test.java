3db56819530f3b6641df753ceeb7dd255b17e7e5
Donne <17685811513@163.com>
2020-08-06 18:13:04 +0800

    master-1

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * 2 * @Author: donne
+ * 3 * @Date: 2020/8/6 18:12
+ * 4
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("master-1");
+    }
+}
